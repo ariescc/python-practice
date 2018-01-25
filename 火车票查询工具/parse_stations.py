d77f39fb02c451a3163462ba510cf60746fd0259
ariescc <jiachang@mail.nwpu.edu.cn>
2018-01-25 17:14:32 +0800

    解析车站

--- 火车票查询工具/parse_stations.py
+++ 火车票查询工具/parse_stations.py
@@ -11,7 +11,7 @@ from pprint import pprint
 
 url = 'https://kyfw.12306.cn/otn/resources/js/framework/station_name.js?station_version=1.8971'
 
-response = requests.get(url, verify = False)
+response = requests.get(url, verify = False) # verify = False添加参数禁止证书验证
 stations = re.findall(u'([\u4e00-\u9fa5]+)\|([A-Z]+)', response.text)
-
-pprint(dict(stations), indent=4)
+print(dict(stations).keys())
+print(dict(stations).values())
